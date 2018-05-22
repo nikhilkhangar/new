dce4851b1fbce7a5fa970263c0131225b637c568
nikhilkhangar <khangar.nikhil@gmail.com>
2018-05-22 15:28:41 +0530

    Update pushcrew-sw.js

--- pushcrew-sw.js
+++ pushcrew-sw.js
@@ -1,2 +1,2 @@
 var version = 6;
-importScripts('https://cdn.pushcrew.com/sw/0613a5aaed3ec37fc2b53dd98d567c35.js');
+importScripts('https://cdn.pushcrewstaging.com/sw/d349ab40f821b13b4b10840cd52eeb75.js');
